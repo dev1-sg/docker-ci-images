ff9bae16f90fdb46a491782a2894f942e8ebd200
antyung47 <antyung47@gmail.com>
2025-05-25 13:42:22 +0000

    fix python, fix ansible, add packer, add kubectl

---
+++ tests/packer_test.go
@@ -0,0 +1,57 @@
+package tests
+
+import (
+	"context"
+	"io"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"github.com/testcontainers/testcontainers-go"
+)
+
+var Packer = struct {
+	AWS_DEFAULT_REGION string
+	AWS_ECR_PUBLIC_URI string
+	DOCKER_IMAGE_GROUP string
+	DOCKER_IMAGE       string
+	DOCKER_IMAGE_TAG   string
+}{
+	AWS_DEFAULT_REGION: "us-east-1",
+	AWS_ECR_PUBLIC_URI: "public.ecr.aws/dev1-sg",
+	DOCKER_IMAGE_GROUP: "ci",
+	DOCKER_IMAGE:       "packer",
+	DOCKER_IMAGE_TAG:   "latest",
+}
+
+func TestContainersGoPullPacker(t *testing.T) {
+	ctx := context.Background()
+	container, e := testcontainers.GenericContainer(ctx, testcontainers.GenericContainerRequest{
+		ContainerRequest: testcontainers.ContainerRequest{
+			Image: Packer.AWS_ECR_PUBLIC_URI + "/" + Packer.DOCKER_IMAGE_GROUP + "/" + Packer.DOCKER_IMAGE + ":" + Packer.DOCKER_IMAGE_TAG,
+		},
+	})
+	require.NoError(t, e)
+	container.Terminate(ctx)
+}
+
+func TestContainersGoExecPacker(t *testing.T) {
+	ctx := context.Background()
+	container, e := testcontainers.GenericContainer(ctx, testcontainers.GenericContainerRequest{
+		ContainerRequest: testcontainers.ContainerRequest{
+			Image: Packer.AWS_ECR_PUBLIC_URI + "/" + Packer.DOCKER_IMAGE_GROUP + "/" + Packer.DOCKER_IMAGE + ":" + Packer.DOCKER_IMAGE_TAG,
+			Cmd:   []string{"sleep", "10"},
+		},
+		Started: true,
+	})
+	require.NoError(t, e)
+	defer container.Terminate(ctx)
+
+	exitCode, reader, e := container.Exec(ctx, []string{"packer", "--version"})
+	require.NoError(t, e)
+	require.Equal(t, 0, exitCode)
+
+	output, e := io.ReadAll(reader)
+	require.NoError(t, e)
+
+	require.Contains(t, string(output), "packer", "Expected output not found")
+}
