2bab8b79955d12014b076cbc5c6cc7d782c133c3
antyung47 <antyung47@gmail.com>
2025-06-15 15:27:19 +0800

    project update

--- setup.sh
+++ setup.sh
@@ -3,6 +3,6 @@
 git submodule update --init --recursive \
 && cd shared && git pull origin main && cd ..
 
-python -m venv .venv \
+python3 -m venv .venv \
 && source .venv/bin/activate \
 && pip install -r requirements.txt
