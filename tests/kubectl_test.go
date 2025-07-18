4b30c5ace6b13b90ae78f0b68c71dc753ec93444
antyung47 <antyung47@gmail.com>
2025-07-19 02:22:09 +0800

    add kubectl

---
+++ tests/kubectl_test.go
@@ -0,0 +1,69 @@
+package tests
+
+import (
+	"context"
+	"io"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"github.com/testcontainers/testcontainers-go"
+)
+
+var Kubectl = struct {
+	AWS_ECR_PUBLIC_REGION           string
+	AWS_ECR_PUBLIC_URI              string
+	AWS_ECR_PUBLIC_REPOSITORY_GROUP string
+	AWS_ECR_PUBLIC_IMAGE_NAME       string
+	AWS_ECR_PUBLIC_IMAGE_TAG        string
+}{
+	AWS_ECR_PUBLIC_REGION:           "us-east-1",
+	AWS_ECR_PUBLIC_URI:              "public.ecr.aws/dev1-sg",
+	AWS_ECR_PUBLIC_REPOSITORY_GROUP: "ci",
+	AWS_ECR_PUBLIC_IMAGE_NAME:       "kubectl",
+	AWS_ECR_PUBLIC_IMAGE_TAG:        "latest",
+}
+
+func TestContainersGoPullKubectl(t *testing.T) {
+	ctx := context.Background()
+	for attempt := 0; attempt < 3; attempt++ {
+		container, e := testcontainers.GenericContainer(ctx, testcontainers.GenericContainerRequest{
+			ContainerRequest: testcontainers.ContainerRequest{
+				Image: Kubectl.AWS_ECR_PUBLIC_URI + "/" + Kubectl.AWS_ECR_PUBLIC_REPOSITORY_GROUP + "/" + Kubectl.AWS_ECR_PUBLIC_IMAGE_NAME + ":" + Kubectl.AWS_ECR_PUBLIC_IMAGE_TAG,
+			},
+		})
+		require.NoError(t, e)
+		container.Terminate(ctx)
+	}
+}
+
+func TestContainersGoExecKubectl(t *testing.T) {
+	ctx := context.Background()
+	container, e := testcontainers.GenericContainer(ctx, testcontainers.GenericContainerRequest{
+		ContainerRequest: testcontainers.ContainerRequest{
+			Image: Kubectl.AWS_ECR_PUBLIC_URI + "/" + Kubectl.AWS_ECR_PUBLIC_REPOSITORY_GROUP + "/" + Kubectl.AWS_ECR_PUBLIC_IMAGE_NAME + ":" + Kubectl.AWS_ECR_PUBLIC_IMAGE_TAG,
+			Cmd:   []string{"sleep", "10"},
+		},
+		Started: true,
+	})
+	require.NoError(t, e)
+	defer container.Terminate(ctx)
+
+	commands := [][]string{
+		{"aws", "--version"},
+		{"kubectl", "version"},
+		{"helm", "version"},
+		{"kustomize", "version"},
+	}
+
+	for _, cmd := range commands {
+		exitCode, reader, e := container.Exec(ctx, cmd)
+		require.NoError(t, e)
+		require.Equal(t, 0, exitCode)
+
+		output, e := io.ReadAll(reader)
+		require.NoError(t, e)
+
+		t.Logf("Command: %v\nOutput: %s\n", cmd, output)
+		require.NotEmpty(t, output)
+	}
+}
